65d55bbf1aef6ead58b509ab0541230c75117f47
thatguykelly <kw@kellywilt.com>
2023-09-15 12:22:07 -0400

    second commit

---
+++ src/environments/environment.prod.ts
@@ -0,0 +1,6 @@
+export const environment = {
+  production: true,
+  // api_url: 'https://q-a-example-loopback-api.herokuapp.com'
+
+  api_url: 'https://8lgb2ck0z5.execute-api.us-east-1.amazonaws.com'
+};
